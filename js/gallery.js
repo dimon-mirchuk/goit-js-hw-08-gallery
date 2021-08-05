6b620c5a96add896615834e99ad1beb10e7e2d3a
Dmitry Mirchuk <79853644+dimon-mirchuk@users.noreply.github.com>
2021-08-05 12:43:57 +0300

    Добавил пролистывание изображений

--- js/gallery.js
+++ js/gallery.js
@@ -25,7 +25,6 @@ const modalOpen = (evt) => {
     const originalImg = imgRef.dataset.source;
     modalImage.src = originalImg;
     modalImage.alt = imgRef.alt;
-    modalImage.dataset.index = imgRef.dataset.index;
     modalBox.classList.add("is-open");
 };
 
@@ -34,10 +33,43 @@ imagesGallery.addEventListener("click", modalOpen);
 
 const closeModal = (evt) => {
     modalBox.classList.remove("is-open");
-    modalImage.src = " ";
-    modalImage.alt = " ";
-    modalImage.dataset.index = " ";
+    modalImage.src = "";
+    modalImage.alt = "";
 };
 
 closeModalBtn.addEventListener("click", closeModal);
 overlayCloseModal.addEventListener("click", closeModal);
+
+
+const keyPressOpt = (evt) => {
+  const esc = 'Escape';
+  const arrowRight = 'ArrowRight';
+  const arrowLeft = 'ArrowLeft';
+  const originalImagesArr = galleryItems.map(({ original }) => original);
+  let currentImage = originalImagesArr.indexOf(modalImage.src);
+    
+  switch (evt.code) {
+    case esc:
+      closeModal();
+      break;
+    case arrowRight:
+      nextImage();
+      break;
+    case arrowLeft:
+      previousImage();
+      break;
+  }
+
+  function previousImage() {
+    currentImage === 0 ? (currentImage = originalImagesArr.length - 1) : (currentImage -= 1);
+    modalImage.src = originalImagesArr[currentImage];
+    modalImage.alt = originalImagesArr[currentImage].description;
+}
+function nextImage() {
+  currentImage === originalImagesArr.length - 1 ? (currentImage = 0) : (currentImage += 1);
+  modalImage.src = originalImagesArr[currentImage];
+  modalImage.alt = originalImagesArr[currentImage].description;
+}
+}
+    
+document.addEventListener("keydown", keyPressOpt);
